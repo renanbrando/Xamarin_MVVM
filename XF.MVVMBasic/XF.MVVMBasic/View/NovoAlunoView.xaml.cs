e07f7b59829cc7bb58c2a661b27025dac6112388
renan.brando <reebrando@gmail.com>
2017-11-21 15:20:03 -0200

    Added exercise files.

---
+++ XF.MVVMBasic/XF.MVVMBasic/View/NovoAlunoView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XF.MVVMBasic.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NovoAlunoView : ContentPage
+    {
+        public NovoAlunoView()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            txtNome.Text = txtRM.Text = txtEmail.Text = string.Empty;
+        }
+    }
+}
